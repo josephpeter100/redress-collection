1eb1bacdff60d1086aa8e15652d4349bfa231805
Joseph Peter <oilejoseph1@gmail.com>
2025-12-14 14:03:23 +0300

    Add Redress Collection pink theme and layout styles

--- src/data/products.js
+++ src/data/products.js
@@ -4,18 +4,21 @@ const products = [
     name: "Pink Silk Dress",
     price: 650,
     image: "https://i.imgur.com/eN9lueN.jpeg",
+    category: "Dresses",
   },
   {
     id: 2,
     name: "Soft White Top",
     price: 300,
     image: "https://i.imgur.com/3qWcLxg.jpeg",
+    category: "Tops",
   },
   {
     id: 3,
-    name: "Mini Shoulder Bag",
+    name: "Shoulder Bag",
     price: 500,
     image: "https://i.imgur.com/Yx2Jssz.jpeg",
+    category: "Bags",
   },
 ];
 
